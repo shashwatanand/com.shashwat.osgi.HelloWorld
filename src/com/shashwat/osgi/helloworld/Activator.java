65aa0df0ea3c88bf1105bc8243b48bd74461794a
Shashwat Anand <shashwat.anand@gmail.com>
2015-01-10 13:50:37 +0530

    Modified the activator and manifest file

--- src/com/shashwat/osgi/helloworld/Activator.java
+++ src/com/shashwat/osgi/helloworld/Activator.java
@@ -2,10 +2,15 @@ package com.shashwat.osgi.helloworld;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+
+import com.shashwat.osgi.helloservice.service.HelloService;
 
 public class Activator implements BundleActivator {
 
 	private static BundleContext context;
+	
+	ServiceReference<?> serviceReference;
 
 	static BundleContext getContext() {
 		return context;
@@ -18,6 +23,9 @@ public class Activator implements BundleActivator {
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
 		System.out.println("Starting Hello world OSGi Bundle");
+		serviceReference = context.getServiceReference(HelloService.class.getName());
+		HelloService service = (HelloService)context.getService(serviceReference);
+		System.out.println(service.greetHello());
 	}
 
 	/*
@@ -26,6 +34,7 @@ public class Activator implements BundleActivator {
 	 */
 	public void stop(BundleContext bundleContext) throws Exception {
 		System.out.println("Stopping Hello world OSGi Bundle");
+		context.ungetService(serviceReference);
 		Activator.context = null;
 	}
 }
